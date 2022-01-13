404488f39c4669c4919806c268df8208df8c0cdd
elidupree <vcs@elidupree.com>
2022-01-13 11:32:31 -0500

    got supervisor working

--- supervisor/supervisor.py
+++ supervisor/supervisor.py
@@ -1,4 +1,5 @@
 import os
+import socket
 import json
 from dotenv import load_dotenv
 
@@ -6,7 +7,7 @@ load_dotenv()
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((os.environ["EMG_SERVER_IP"], os.environ["EMG_SERVER_PORT"]))
+s.connect((os.environ["EMG_SERVER_IP"], int(os.environ["EMG_SERVER_PORT"])))
 
 for line in s.makefile():
     print(json.loads(line))
