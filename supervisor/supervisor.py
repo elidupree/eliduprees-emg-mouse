246b0d47e19f05434657261805dd1a447367a83d
elidupree <vcs@elidupree.com>
2022-01-17 08:55:42 -0500

    messed with supervisor (added experimental graph)

--- supervisor/supervisor.py
+++ supervisor/supervisor.py
@@ -4,6 +4,10 @@ import json
 import pyglet
 import pyautogui
 from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+
 
 load_dotenv()
 
@@ -17,16 +21,40 @@ mouse_pressed = False
 click_threshold = 500
 unclick_threshold = 200
 
+memory_count = 200
+left_button_memory = [0]* memory_count
+
+# plt.style.use('ggplot')
+# plt.ion()
+# fig = plt.figure(figsize=(13,6))
+# ax = fig.add_subplot(111)
+# left_button_line, = ax.plot(list(range (memory_count)),left_button_memory,'-o')
+# plt.ylabel('mV I think')
+# plt.ylim([0, 2500])
+# plt.show()
+
+total_inputs = 0
+start = datetime.now()
+
 for line in s.makefile():
     data = json.loads(line)
     print(data)
     if mouse_pressed:
         if data["left_button"] < unclick_threshold:
-            pyautogui.mouseUp()
+            #pyautogui.mouseUp()
             unclick_sound.play()
             mouse_pressed = False
     else:
         if data["left_button"] > click_threshold:
-            pyautogui.mouseDown()
+            #pyautogui.mouseDown()
             click_sound.play()
             mouse_pressed = True
+
+    total_inputs += 1
+    now = datetime.now()
+    #print (f"{total_inputs}: {total_inputs/(now- start).total_seconds()}")
+
+    left_button_memory = left_button_memory[1:] + [data["left_button"]]
+    # if total_inputs % 100 == 0:
+    #     left_button_line.set_ydata(left_button_memory)
+    #     plt.pause(0.001)
