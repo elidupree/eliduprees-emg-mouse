71db58f3d088d483d07587291d102c332cf35e7f
elidupree <vcs@elidupree.com>
2022-02-28 09:28:37 -0500

    Relay stderr through script

--- scripts/rerun_on_x_error.py
+++ scripts/rerun_on_x_error.py
@@ -2,9 +2,21 @@ import sys
 import subprocess
 
 while True:
-    result = subprocess.run (sys.argv[1:], stderr=subprocess.PIPE)
-    if result.returncode == 1 and b"X Error" in result.stderr:
-        print("Crashed due to X Error, re-running")
+    child = subprocess.Popen(sys.argv[1:], stderr=subprocess.PIPE)
+    stderr_chunks = []
+    while True:
+        data = child.stderr.read(4096)
+        stderr_chunks.append (data)
+        sys.stderr.buffer.write(data)
+        sys.stderr.flush()
+        if not data:
+            break
+
+    #print("\nChild stderr closed.")
+    child.wait()
+    #print("Child exited.")
+    if child.returncode == 1 and b"X Error" in b"".join(stderr_chunks):
+        print("Crashed due to X Error, re-running...")
         continue
     else:
         break
